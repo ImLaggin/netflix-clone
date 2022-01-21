81eadfab0c1daf7103218d3fcf21cd6d6c06627f
ImLaggin <msakshay2001@gmail.com>
2022-01-21 21:14:17 +0530

    added the signout

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
-import React from 'react'
-import './App.css';
-import Row from './Row'
-import requests from './requests'
-import Banner from './Banner'
-import Nav from './Nav'
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
 
+//aws
+import Amplify from "aws-amplify";
+
+import awsconfig from "./aws-exports";
+import { Auth } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(awsconfig);
+Auth.configure(awsconfig);
 
 function App() {
   return (
-    
+    <div>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <div className="App">
+            <Nav signOut={signOut} />
+            <Banner />
 
-    //HeaderNav
-    <div className="App">
-        <Nav />
-        <Banner />
-        <Row 
-        title = "NETFLIX ORIGINALS" 
-        fetchUrl = {requests.fetchNetFlixOriginals}
-        isLargeRow = {true}
-        />
-        <Row title = "Trending Now" fetchUrl = {requests.fetchTreding}/>
-        <Row title = "Top Rated" fetchUrl = {requests.fetchTopRated}/>
-        <Row title = "Action Movies" fetchUrl = {requests.fetchActionMovies}/>
-        <Row title = "Horror Movies" fetchUrl = {requests.fetchHorroMovies}/>
-        <Row title = "Romance Movies" fetchUrl = {requests.fetchRomanticMovies}/>
-        <Row title = "Documentaries" fetchUrl = {requests.fetchDocumentaries}/>
+            <Row
+              title="NETFLIX ORIGINALS"
+              fetchUrl={requests.fetchNetFlixOriginals}
+              isLargeRow={true}
+            />
+            <Row title="Trending Now" fetchUrl={requests.fetchTreding} />
+            <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+            <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+            <Row title="Horror Movies" fetchUrl={requests.fetchHorroMovies} />
+            <Row
+              title="Romance Movies"
+              fetchUrl={requests.fetchRomanticMovies}
+            />
+            <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+          </div>
+        )}
+      </Authenticator>
     </div>
   );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -1,36 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import "./Nav.css"
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
 
-function Nav() {
+function Nav(props) {
+  const [show, handleShow] = useState(false);
 
-    const [show, handleShow] = useState(false);
+  useEffect(() => {
+    const scrollListener = window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll", scrollListener);
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <div>
+        <img
+          className="nav__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+          alt="Netflix Logo"
+        />
+      </div>
 
-
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            if(window.scrollY > 100) {
-                handleShow(true);
-            } else handleShow(false);
-        })
-        return () =>{
-            window.removeEventListener("scroll")    
-        }
-    }, [])
-    return (
-        <div className = {`nav ${ show && "nav__black" }`}>
-
-            <img className = "nav__logo"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
-                alt="Netflix Logo"
-            />
-
-            <img className = "nav__avtar"
-                src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
-                alt="Netflix Logo"
-            />
-            
-        </div>
-    )
+      {/* <img
+          className="nav__avtar"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+          alt="Netflix Logo"
+        /> */}
+      <div>
+        <button onClick={props.signOut} className="signout_button">
+          Sign out
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
